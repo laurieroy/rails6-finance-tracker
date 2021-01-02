73bebe56b5aa1b7b4ce71d45edc08aaf95a2b030
lauretteroy <laurieroy.dev@gmail.com>
2021-01-01 17:53:28 -0700

    Add 10 stocks, already tracked limits; del assoc

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -5,8 +5,17 @@ class UserStocksController < ApplicationController
 			stock = Stock.new_lookup(params[:ticker])
 			stock.save
 		end
+
 		@user_stock = UserStock.create(user: current_user, stock: stock)
-		flash[:notice] = "#{stock.name} was successfully added to your portfolio"
+		flash.now[:notice] = "#{stock.name} was successfully added to your portfolio"
+		redirect_to my_portfolio_path
+	end
+
+	def destroy
+		stock = Stock.find(params[:id])
+		user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+		user_stock.destroy
+		flash.now[:notice] = "#{stock.ticker} was successfully removed from your portfolio"
 		redirect_to my_portfolio_path
 	end
 
