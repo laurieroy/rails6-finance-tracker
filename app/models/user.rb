3c6c6dd8885963695cad88231cea7d61b2176900
lauretteroy <laurieroy.dev@gmail.com>
2021-01-25 15:09:01 -0700

    Add search string

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,27 +8,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def under_stock_limit?
-    stocks.count < 10
-  end
-
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
-  end
-  
-  def stock_already_tracked?(ticker_symbol)
-    stock = Stock.check_db(ticker_symbol)
-    return false unless stock
-    stocks.where(id: stock.id).exists?
-  end
-
-  def can_track_stock?(ticker_symbol)
-    under_stock_limit?
-  end
-
-  def full_name
-    return "#{first_name} #{last_name}" if first_name || last_name
-    "Anonymous"
   end
 
   def friend_already_tracked?(ticker_symbol)
@@ -36,5 +17,43 @@ class User < ApplicationRecord
     return false unless stock
     stocks.where(id: stock.id).exists?
     
+  end
+
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def self.matches_first_name(param)
+    matches('first_name', param)
+  end
+
+  def self.matches_email(param)
+    matches('email', param)
+  end
+
+  def self.matches_last_name(param)
+    matches('last_name', param)
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (matches_first_name(param) + matches_last_name(param) + matches_email(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+
+  def under_stock_limit?
+    stocks.count < 10
   end
 end
